54ce2e0e64a82cb70f695c650e27bb3f3d09194a
kirill.gora <45240428+Elkirillo@users.noreply.github.com>
2020-02-23 20:17:49 +0300

    Update dtsinfo.cpp

--- dtsinfo.cpp
+++ dtsinfo.cpp
@@ -3,11 +3,12 @@
 int main (int argc, char** argv)
 {
 	DTS* arr[4]={new passBus(), new repairBus(), new miniBus(), new tractor()};
+	//заполнение информацией
 	arr[0]->filling();
 	arr[1]->filling();
 	arr[2]->filling();
 	arr[3]->filling();
-
+	//вывод информации
 	arr[0]->Getinfo();
 	arr[1]->Getinfo();
 	arr[2]->Getinfo();
