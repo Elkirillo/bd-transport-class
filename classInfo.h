dd6334398209125ead32a77b9e1e0516190fd313
kirill.gora <45240428+Elkirillo@users.noreply.github.com>
2020-02-24 20:04:59 +0300

    Update classInfo.h

--- classInfo.h
+++ classInfo.h
@@ -7,61 +7,61 @@ using std::string;
 //Базовый класс "дорожно транспортное средство"
 class DTS 
 {
-	public: 
-		DTS ();
-		//виртуальный метод, который реализует интерфейс заполнения данными
-		virtual void filling() =0 ;
-        	//виртуальный метод, который выводит информацию об требуемом транспорте
-		virtual void Getinfo() = 0;
-		virtual ~DTS ();
-	protected:
-		int number;
-		string brand;
+public: 
+	DTS ();
+	//виртуальный метод, который реализует интерфейс заполнения данными
+	virtual void filling() =0 ;
+        //виртуальный метод, который выводит информацию об требуемом транспорте
+	virtual void Getinfo() = 0;
+	virtual ~DTS ();
+protected:
+	int number;
+	string brand;
 		
 };
 //класс, производный от базового класса, "пассажирский автобус"
 class passBus : public DTS 
 {
-	public:
-		passBus ();
-		//метод заполнения информацией об данном транспорте
-		void filling () override;
-		//метод, который выводит информацию о транспорте
-		void Getinfo() override;
-		~passBus () override;
+public:
+	passBus ();
+	//метод заполнения информацией об данном транспорте
+	void filling () override;
+	//метод, который выводит информацию о транспорте
+	void Getinfo() override;
+	~passBus () override;
 	
 };
 //класс, производный от базового класса, "ремонтный автобус"
 class repairBus : public DTS 
 {
-	public:
-		repairBus ();
-		//метод заполнения информацией об данном транспорте
-		void filling () override;
-		//метод, который выводит информацию о транспорте
-		void Getinfo() override;
-		~repairBus () override;
+public:
+	repairBus ();
+	//метод заполнения информацией об данном транспорте
+	void filling () override;
+	//метод, который выводит информацию о транспорте
+	void Getinfo() override;
+	~repairBus () override;
 };
 //класс, производный от базового класса, "микро-автобус"
 class miniBus : public DTS 
 {
-	public:
-		miniBus ();
-		//метод заполнения информацией об данном транспорте
-		void filling () override;
-		//метод, который выводит информацию о транспорте
-		void Getinfo() override;
-		~miniBus () override;
+public:
+	miniBus ();
+	//метод заполнения информацией об данном транспорте
+	void filling () override;
+	//метод, который выводит информацию о транспорте
+	void Getinfo() override;
+	~miniBus () override;
 };
 //класс, производный от базового класса, "трактор"
 class tractor : public DTS 
 {
-	public:
-		tractor ();
-		//метод заполнения информацией об данном транспорте
-		void filling () override;
-		//метод, который выводит информацию о транспорте
-		void Getinfo() override;
-		~tractor () override;
+public:
+	tractor ();
+	//метод заполнения информацией об данном транспорте
+	void filling () override;
+	//метод, который выводит информацию о транспорте
+	void Getinfo() override;
+	~tractor () override;
 	
 };
